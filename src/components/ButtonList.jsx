ec336d38af2badbdf2ce2c8a7248f576a7da14de
Shashwat20-create <shashwatsahay1999@gmail.com>
2024-06-08 00:45:00 +0530

    build youtube and live chat

---
+++ src/components/ButtonList.jsx
@@ -0,0 +1,26 @@
+import Button from "./Button";
+const list = [
+  "All",
+  "Gaming",
+  "Songs",
+  "Live",
+  "Soccer",
+  "Cricket",
+  "Cooking",
+  "Valentines",
+  "Books",
+  "Dance",
+  "Nature",
+  "Relationship",
+];
+const ButtonList = () => {
+  return (
+    <div className="flex">
+      {list.map((button, index) => (
+        <Button key={index} name={button} />
+      ))}
+    </div>
+  );
+};
+
+export default ButtonList;
